b83b0e3a9d722be5da5bdb79db4f5b1219d278f5
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-11-05 02:18:51 +1000

    курсовая

--- src/main/java/pro/sky/homework_generator/Exception/MaxLimitRequestOfQuestions.java
+++
@@ -1,9 +0,0 @@
-package pro.sky.homework_generator.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class MaxLimitRequestOfQuestions extends RuntimeException {
-
-}
